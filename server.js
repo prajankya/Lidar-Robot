bfa96d7772ac897fb51cbab306dd676cf6887eae
prajankya <prajankya@gmail.com>
2016-06-14 18:24:30 +0530

    Added ROS control center, Repaired the dependancy on port for index.html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var server = express();
 server.use(express.static(__dirname + '/'));
  
-var port = 8080;//Also change in index.html
+var port = 8080;
 server.listen(port, function() {
     console.log('server listening on port ' + port);
 });
