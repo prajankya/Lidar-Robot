655b4a988438cc85e4c4372820ef54e540e251d1
prajankya <prajankya@gmail.com>
2016-06-16 09:10:03 +0530

    Updated server.js for spawn, printing stdout and stderr now

--- webserver/server.js
+++ webserver/server.js
@@ -1,15 +1,15 @@
 var exec = require('child_process').exec;
 
 var express = require('express');
- 
+
 var server = express();
 server.use(express.static(__dirname + '/../'));
- 
+
 var port = 8080;
 server.listen(port, function() {
     console.log('server listening on port ' + port);
 });
-
+/*
 function puts(err, out, code) {
   if (err instanceof Error)
     throw err;
@@ -19,3 +19,18 @@ function puts(err, out, code) {
 }
 
 exec('roslaunch lidar_robot server.launch', puts);
+*/
+
+var child = require('child_process').spawn('roslaunch', ['lidar_robot', 'server.launch']);
+
+child.stdout.on('data', function (data) {
+  console.log('stdout: ' + data);
+});
+
+child.stderr.on('data', function (data) {
+  console.log('stderr: ' + data);
+});
+
+child.on('close', function (code) {
+    console.log('child process exited with code ' + code);
+});
